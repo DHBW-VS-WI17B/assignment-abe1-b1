1b56913fb02e8298f53eae63db927d17a17fa007
Robin <mail@robingenz.de>
2020-08-15 16:31:07 +0200

    feat(server): implement cli (#16)

--- src/server/app/__main__.py
+++ src/server/app/__main__.py
@@ -1,7 +1,22 @@
+"""Server CLI
+Usage:
+    server
+    server start [--host=<ip>] [--port=<port>]
+    server -h|--help
+    server -v|--version
+
+Options:
+    --host=<ip>     IP address [default: 0.0.0.0].
+    --port=<port>   Port [default: 8080].
+    -h --help       Show this screen.
+    -v --version    Show version.
+"""
+
 import signal
 import sys
 from flask import Flask
 from thespian.actors import ActorSystem
+from docopt import docopt
 from gevent import pywsgi
 
 
@@ -15,11 +30,15 @@ def main(args):
     app = Flask(__name__)
     # app.register_blueprint(events.bp)
     ActorSystem('multiprocTCPBase')
-    msg = "\033[92mServer is listening...\033[0m"
+    msg = "\033[92mServer is listening on {}:{} ...\033[0m"
+    print(msg.format(args.get('--host'), args.get('--port')))
     print("\033[93mUse Ctrl+C to quit this process.\033[0m")
-    server = pywsgi.WSGIServer(('', 8080)), app)
+    server = pywsgi.WSGIServer(
+        (args.get('--host'), int(args.get('--port'))), app)
     server.serve_forever()
 
 
 if __name__ == '__main__':
-    main()
+    args = docopt(__doc__, version='1.0.0')
+    if args.get('start') == True:
+        main(args)
