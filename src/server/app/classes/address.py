d8f6ad7b9fc5f175561a2ba6f20028f03370748d
Robin <mail@robingenz.de>
2020-08-16 18:46:29 +0200

    feat(server): add entity classes (#20)

---
+++ src/server/app/classes/address.py
@@ -0,0 +1,7 @@
+class Address:
+    def __init__(self, name, street, zip, city, street_number):
+        self.name = name
+        self.street = street
+        self.zip = zip
+        self.city = city
+        self.street_number = street_number
