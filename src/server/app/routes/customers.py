8ce03a440914a3aa551627caa7fa38ddc2d1911f
Elias <ghost.214@web.de>
2020-08-17 18:37:12 +0200

    fix lint errors

--- src/server/app/routes/customers.py
+++ src/server/app/routes/customers.py
@@ -1,18 +1,20 @@
+""" Customer Actor """
 from flask import Blueprint
 from flask import request
 from flask import jsonify
 from thespian.actors import ActorSystem
-from app.enums.actor_name import ActorName
-from app.enums.customers_actor_action import CustomersActorAction
-from app.actors.customers_actor import CustomersActor
-from app.classes.customer import Customer
-from app.classes.actor_message import ActorMessage
+from server.app.enums.actor_name import ActorName
+from server.app.enums.customers_actor_action import CustomersActorAction
+from server.app.actors.customers_actor import CustomersActor
+from server.app.classes.customer import Customer
+from server.app.classes.actor_message import ActorMessage
 
 bp = Blueprint("customers", __name__, url_prefix='/api/customers')
 
 @bp.route("/", methods=["POST"])
-def add(customer_id):
-    try: 
+def add():
+    """ add Customer """
+    try:
         asys = ActorSystem()
         actor = asys.createActor(CustomersActor, None, ActorName.CUSTOMER_ACTOR)
         customer = Customer.from_json(request.get_json())
@@ -22,12 +24,20 @@ def add(customer_id):
         message = ActorMessage(CustomersActorAction.CUSTOMERS_ADD, payload)
         customer = asys.ask(actor, message)
         return "", 204
-    except Exception as e:
-        return jsonify({'error': str(e)})
+    except Exception as ex:
+        return jsonify({'error': str(ex)})
 
 @bp.route("/<customer_id>", methods=["GET"])
 def get(customer_id):
-    try: 
+    """ get customer by id
+
+    Args:
+        customer_id (int): customer Id
+
+    Returns:
+        Customer: Customer that has specific id
+    """
+    try:
         asys = ActorSystem()
         actor = asys.createActor(CustomersActor, None, ActorName.CUSTOMER_ACTOR)
         payload = {
@@ -36,13 +46,21 @@ def get(customer_id):
         message = ActorMessage(CustomersActorAction.CUSTOMERS_GET, payload)
         customer = asys.ask(actor, message)
         return jsonify(customer)
-    except Exception as e:
-        return jsonify({'error': str(e)})
+    except Exception as ex:
+        return jsonify({'error': str(ex)})
 
 
 @bp.route("/<customer_id>/budget", methods=["POST"])
 def get_budget(customer_id):
-    try: 
+    """[summary]
+
+    Args:
+        customer_id (int): customer Id
+
+    Returns:
+        int: budget of the customer
+    """
+    try:
         asys = ActorSystem()
         actor = asys.createActor(CustomersActor, None, ActorName.CUSTOMER_ACTOR)
         payload = {
@@ -51,11 +69,19 @@ def get_budget(customer_id):
         message = ActorMessage(CustomersActorAction.CUSTOMERS_BUDGET, payload)
         budget = asys.ask(actor, message)
         return jsonify(budget)
-    except Exception as e:
-        return jsonify({'error': str(e)})
+    except Exception as ex:
+        return jsonify({'error': str(ex)})
 
 @bp.route("/<customer_id>/tickets", methods=["POST"])
 def get_tickets(customer_id):
+    """ get tickets of the customer
+
+    Args:
+        customer_id (int): customer id
+
+    Returns:
+        Ticket(): List of ticket filtered by order date or event date
+    """
     try:
         asys = ActorSystem()
         actor = asys.createActor(CustomersActor, None, ActorName.CUSTOMER_ACTOR)
@@ -70,5 +96,5 @@ def get_tickets(customer_id):
         for ticket in tickets:
             tickets_dict.append(ticket.__dict__)
         return jsonify(tickets)
-    except Exception as e:
-        return jsonify({'error': str(e)})
+    except Exception as ex:
+        return jsonify({'error': str(ex)})
