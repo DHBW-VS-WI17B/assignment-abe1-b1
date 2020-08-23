471af32cb47a54e4c2e8ba19fa5ddb62daf49e02
Robin Genz <mail@robingenz.de>
2020-08-23 19:53:54 +0000

    remove sale and address classes and models

--- src/server/app/models/sale.py
+++
@@ -1,9 +0,0 @@
-from peewee import DateField, IntegerField, AutoField
-from app.models.base_model import BaseModel
-
-
-class Sale(BaseModel):
-    # TODO
-    id = AutoField()
-    start_date = DateField()
-    period = IntegerField()
