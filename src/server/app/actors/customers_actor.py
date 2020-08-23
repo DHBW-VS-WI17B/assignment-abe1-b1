c8c7a12eb3b341c2dd49dc25d8bf10affee34d1d
Robin Genz <mail@robingenz.de>
2020-08-23 15:28:00 +0000

    update events and customers actor

--- src/server/app/actors/customers_actor.py
+++ src/server/app/actors/customers_actor.py
@@ -1,4 +1,6 @@
 from thespian.actors import Actor
+from app.actors.db_actor import DbActor
+from app.enums.actor_name import ActorName
 from app.enums.customers_action import CustomersActorAction
 from app.classes.actor_message import ActorMessage
 
@@ -8,13 +10,19 @@ class CustomersActor(Actor):
     def receiveMessage(self, msg, sender):
         if not isinstance(msg, ActorMessage):
             return
-        action = msg.action
-        payload = msg.payload
-        customer_id = msg.customer_id
-        response_to = msg.response_to
-        if action == CustomersActorAction.CUSTOMERS_ADD:
-            print('TODO')
-        if action == CustomersActorAction.CUSTOMERS_BUDGET:
-            print('TODO')
-        if action == CustomersActorAction.CUSTOMERS_TICKETS:
-            print('TODO')
+        if msg.action == CustomersActorAction.CUSTOMERS_ADD:
+            db_actor = self.createActor(actorClass=DbActor,
+                                        globalName=ActorName.DB_ACTOR)
+            message = ActorMessage(action=msg.action, payload=msg.payload,
+                                   customer_id=msg.customer_id)
+            self.send(db_actor, message)
+        if msg.action == CustomersActorAction.CUSTOMERS_BUDGET:
+            db_actor = self.createActor(actorClass=DbActor)
+            message = ActorMessage(action=msg.action, payload=msg.payload,
+                                   customer_id=msg.customer_id, response_to=msg.sender)
+            self.send(db_actor, message)
+        if msg.action == CustomersActorAction.CUSTOMERS_TICKETS:
+            db_actor = self.createActor(actorClass=DbActor)
+            message = ActorMessage(action=msg.action, payload=msg.payload,
+                                   customer_id=msg.customer_id, response_to=msg.sender)
+            self.send(db_actor, message)
--- src/server/app/actors/events_actor.py
+++ src/server/app/actors/events_actor.py
@@ -3,8 +3,6 @@ from app.enums.events_action import EventsActorAction
 from app.actors.db_actor import DbActor
 from app.enums.actor_name import ActorName
 from app.classes.actor_message import ActorMessage
-from app.models.event import Event
-from app.config.config import Config
 
 
 class EventsActor(Actor):
@@ -12,34 +10,30 @@ class EventsActor(Actor):
     def receiveMessage(self, msg, sender):
         if not isinstance(msg, ActorMessage):
             return
-        action = msg.action
-        payload = msg.payload
-        customer_id = msg.customer_id
-        response_to = msg.response_to
-        if action == EventsActorAction.EVENTS_ADD:
-            db_actor = self.createActor(
-                actorClass=DbActor, globalName=ActorName.DB_ACTOR)
-            message = ActorMessage(
-                action=action, payload=payload, customer_id=customer_id)
+        if msg.action == EventsActorAction.EVENTS_ADD:
+            db_actor = self.createActor(actorClass=DbActor,
+                                        globalName=ActorName.DB_ACTOR)
+            message = ActorMessage(action=msg.action, payload=msg.payload,
+                                   customer_id=msg.customer_id)
             self.send(db_actor, message)
-        if action == EventsActorAction.EVENTS_GET:
+        if msg.action == EventsActorAction.EVENTS_GET:
             db_actor = self.createActor(actorClass=DbActor)
-            message = ActorMessage(
-                action=action, payload=payload, customer_id=customer_id, response_to=sender)
+            message = ActorMessage(action=msg.action, payload=msg.payload,
+                                   customer_id=msg.customer_id, response_to=msg.sender)
             self.send(db_actor, message)
-        if action == EventsActorAction.EVENTS_LIST:
+        if msg.action == EventsActorAction.EVENTS_LIST:
             db_actor = self.createActor(actorClass=DbActor)
-            message = ActorMessage(
-                action=action, payload=payload, customer_id=customer_id, response_to=sender)
+            message = ActorMessage(action=msg.action, payload=msg.payload,
+                                   customer_id=msg.customer_id, response_to=msg.sender)
             self.send(db_actor, message)
-        if action == EventsActorAction.EVENTS_PURCHASE:
-            db_actor = self.createActor(
-                actorClass=DbActor, globalName=ActorName.DB_ACTOR)
-            message = ActorMessage(
-                action=action, payload=payload, customer_id=customer_id)
+        if msg.action == EventsActorAction.EVENTS_PURCHASE:
+            db_actor = self.createActor(actorClass=DbActor,
+                                        globalName=ActorName.DB_ACTOR)
+            message = ActorMessage(action=msg.action, payload=msg.payload,
+                                   customer_id=msg.customer_id)
             self.send(db_actor, message)
-        if action == EventsActorAction.EVENTS_TICKETS:
+        if msg.action == EventsActorAction.EVENTS_TICKETS:
             db_actor = self.createActor(actorClass=DbActor)
-            message = ActorMessage(
-                action=action, payload=payload, customer_id=customer_id, response_to=sender)
+            message = ActorMessage(action=msg.action, payload=msg.payload,
+                                   customer_id=msg.customer_id, response_to=msg.sender)
             self.send(db_actor, message)
