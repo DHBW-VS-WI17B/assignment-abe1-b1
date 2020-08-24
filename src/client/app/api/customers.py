20d56720a95aa09b3fb5bbf1a984eab01cd54584
Elias <ghost.214@web.de>
2020-08-24 15:12:48 +0200

    WIP

--- src/client/app/api/customers.py
+++ src/client/app/api/customers.py
@@ -3,36 +3,36 @@ import json
 from app.classes.customer import Customer
 
 class Customers():
+    @staticmethod
     def get_customer(args):
         # server_addr = ip + port
         server_addr = 'http://' + args.ip + ":" + args.port
-        req = requests.get(server_addr + '/api/customers/' + args.customer_id)
-        # req.json() -> dataclass
+        req = requests.get(server_addr + '/api/customers/' + args.customer_id) 
         # return array of events
         print(req.json())
 
+    @staticmethod
     def add_customer(args):
         customer = Customer(args)
         # server_addr = ip + port
         server_addr = 'http://' + args.ip + ":" + args.port
         req = requests.post(server_addr + '/api/customers', json=customer.__dict__)
-        # req.json() -> dataclass
         # return array of events
-        print(req.json())
+        print(req)
 
+    @staticmethod
     def get_tickets(args):
         params = {'order_date': args.order_date, 'event_date': args.event_date}
         # server_addr = ip + port
         server_addr = 'http://' + args.ip + ":" + args.port
         req = requests.get(server_addr + '/api/customers/' + args.customer_id + '/tickets', params=params)
-        # req.json() -> dataclass
         # return array of events
         print(req.json())
 
+    @staticmethod
     def get_budget(args):
         # server_addr = ip + port
         server_addr = 'http://' + args.ip + ":" + args.port
         req = requests.get(server_addr + '/api/customers/' + args.customer_id + '/budget', params=payload)
-        # req.json() -> dataclass
         # return array of events
         print(req.json())
--- src/client/app/classes/event.py
+++ src/client/app/classes/event.py
@@ -1,18 +1,44 @@
 from dataclasses import dataclass
 from tabulate import tabulate
+from datetime import datetime
 
 @dataclass
 class Event:
     def __init__(self, args):
         self.name = args.name
-        self.date = args.date
+        self.date = Event.date_to_timestamp(args.date)
         self.location = args.location
         self.max_tickets = args.max_tickets
         self.max_tickets_per_customer = args.max_tickets_per_customer
         self.sale_period = args.sale_period
-        self.sale_start_date = args.sale_start_date
-        self.price = args.ticket_price
-        
-    def get_table(self, data):
-        headers = ["Name","Date0", "Location", "Price", "Max Tickets", "Max Tickets per Customer", "Sales Period", "Sale Startdate"]
-        print(tabulate(data.items(), headers=headers, floatfmt=".4f"))
+        self.sale_start_date = Event.date_to_timestamp(args.sale_start_date)
+        self.ticket_price = args.ticket_price
+
+    @staticmethod
+    def print_table(data):
+        table = []
+        for item in data:
+            table.append([
+                item['id'],
+                item['name'], 
+                Event.timestamp_to_date(item['date']), 
+                item['location'], 
+                item['ticket_price'], 
+                item['max_tickets'], 
+                item['max_tickets_per_customer'], 
+                Event.timestamp_to_date(item['sale_start_date']), 
+                item['ticket_price']
+            ])
+        headers = ["ID", "Name", "Date", "Location", "Price in €", "Max Tickets", "Max Tickets per Customer", "Sales Startdate", "Sales Period in Days"]
+        print(tabulate(table, headers=headers, floatfmt=".4f"))
+
+    @staticmethod
+    def date_to_timestamp(date):
+        input_date = datetime.strptime(date, '%d.%m.%Y')
+        timestamp = input_date.timestamp()
+        return timestamp
+
+    @staticmethod    
+    def timestamp_to_date(timestamp):
+        date = datetime.fromtimestamp(timestamp)
+        return date.strftime('%d-%m-%y')
