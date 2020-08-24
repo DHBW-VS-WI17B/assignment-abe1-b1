bfdc7bee7781622c45b5156bfafd281dba6b25bb
Elias <ghost.214@web.de>
2020-08-24 23:07:37 +0200

    validate input

--- src/client/app/__main__.py
+++ src/client/app/__main__.py
@@ -22,14 +22,14 @@ Options:
     --quantity=<number>                 The number of tickets.
     --name=<name>                       The name of the customer or event.
     --date=<date>                       The event date (e.g. 01.01.2020).
-    --location=<location>               The event location (e.g. "Staatsoper Stuttgart").
+    --location=<location>               The event location (e.g. "Friedrich-Ebert-Straße 30 78054 Villingen-Schwenningen").
     --ticket-price=<number>             The ticket price in € (e.g. 5).
     --max-tickets=<number>              The maximum number of tickets (e.g. 100).
     --max-tickets-per-customer=<number> The maximum number of tickets per customer (e.g. 3).
     --sale-start-date=<date>            The sale start date (e.g. 01.01.2020).
     --sale-period=<number>              The sale period in days (e.g. 5).
     --budget=<budget>                   The customer budget in € (e.g. 100).
-    --address=<address>                 The customer address (e.g. "Friedrich-Ebert-Straße 30, 78054 Villingen-Schwenningen").
+    --address=<address>                 The customer address (e.g. "Friedrich-Ebert-Straße 30 78054 Villingen-Schwenningen").
     -h, --help                          Show this screen.
     -v, --version                       Show version.
 """
@@ -38,6 +38,8 @@ from docopt import docopt
 from app.classes.arguments import Arguments
 from app.api.events import Events
 from app.api.customers import Customers
+from schema import Schema, And, Or, Use, SchemaError, Regex
+from datetime import datetime
 
 def main(args):
     if args.customer:
@@ -79,5 +81,26 @@ def main(args):
 
 
 if __name__ == '__main__':
-    params = docopt(__doc__, version='1.0.0')
-    main(Arguments(params))
+    args = docopt(__doc__, version='1.0.0')
+    schema = Schema({
+        '--ip': And(str, Regex(r'^(?:[0-9]{1,3}\.){3}[0-9]{1,3}$')),
+        #'--port': And(And(Or(int, str), lambda x: x == 4)),
+        '--date': Or(None, And(str, Regex(r'^\s*(3[01]|[12][0-9]|0?[1-9])\.(1[012]|0?[1-9])\.((?:19|20)\d{2})\s*$'))),
+        '--order-date': Or(None, And(str, Regex(r'^\s*(3[01]|[12][0-9]|0?[1-9])\.(1[012]|0?[1-9])\.((?:19|20)\d{2})\s*$'))),
+        '--event-date': Or(None, And(str, Regex(r'^\s*(3[01]|[12][0-9]|0?[1-9])\.(1[012]|0?[1-9])\.((?:19|20)\d{2})\s*$'))),
+        '--quantity': Or(None, And(Use(int))),
+        '--name': Or(None, And(str, Regex(r'^[A-Za-z]+((\s)?((\'|\-|\.)?([A-Za-z])+))*$'))),
+        '--location': Or(None, And(str, Regex(r'^([^0-9]+) ([0-9]+.*?) ([0-9]{5}) (.*)$'))),
+        '--address': Or(None, And(str, Regex(r'^([^0-9]+) ([0-9]+.*?) ([0-9]{5}) (.*)$'))),
+        '--ticket-price': Or(None, And(Use(int))),
+        '--max-tickets': Or(None, And(Use(int))),
+        '--max-tickets-per-customer': Or(None, And(Use(int))),
+        '--sale-start-date': Or(None, And(str, Regex(r'^\s*(3[01]|[12][0-9]|0?[1-9])\.(1[012]|0?[1-9])\.((?:19|20)\d{2})\s*$'))),
+        '--sale-period': Or(None, And(Use(int))),
+        '--budget': Or(None, And(Use(int))),
+        })
+    try:
+        args = schema.validate(args)
+    except SchemaError as e:
+        exit(e)
+    main(Arguments(args))
