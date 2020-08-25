43f2fbe62e1993f0868548597eb4f3611d31cba7
Robin <mail@robingenz.de>
2020-08-25 20:34:21 +0200

    feat(server): implement actor system (#32)
    * WIP
    
    * add --exit-zero flag to pylint
    
    * WIP
    
    * remove client actor
    
    * add config class
    
    * WIP
    
    * add --db-path option
    
    * WIP
    
    * WIP
    
    * WIP
    
    * WIP
    
    * WIP
    
    * WIP(db_actor): move code in private methods
    
    * WIP: elif
    
    * update events and customers actor
    
    * fix events and customers actor
    
    * WIP
    
    * set correct http status codes
    
    * WIP: tell -> ask
    
    * add sqlite3 to dockerfile
    
    * WIP
    
    * db_actor: global try catch
    
    * update error messages
    
    * fix datetime / date
    
    * fix budget update
    
    * remove import
    
    * remove sale and address classes and models
    
    * add customer tickets filter
    
    * WIP: comments
    
    * WIP
    
    * customer budget: add year param
    
    * use peewee joins and foreign keys
    
    * raise exception if event not found
    
    * fix: get budget
    
    * add event sales interface
    
    * improve exception handling
    
    * WIP
    
    * rename ActorName to ActorGlobalName
    
    * Update db_actor.py
    
    * Update events.py
    
    Bei der quantity wurde die immer als string interpretiert, egal wie ich es in die JSON gegeben habe.
    
    * fix
    
    * fix pylint warnings
    
    Co-authored-by: Bielzelle214 <50101153+Bielzelle214@users.noreply.github.com>

---
+++ src/server/app/enums/events_action.py
@@ -0,0 +1,10 @@
+from enum import Enum, unique
+
+
+@unique
+class EventsActorAction(Enum):
+    EVENTS_LIST = 'EVENTS_LIST'
+    EVENTS_ADD = 'EVENTS_ADD'
+    EVENTS_GET = 'EVENTS_GET'
+    EVENTS_SALES = 'EVENTS_SALES'
+    EVENTS_PURCHASE = 'EVENTS_PURCHASE'
--- src/server/app/routes/events.py
+++ src/server/app/routes/events.py
@@ -2,8 +2,7 @@ from flask import Blueprint
 from flask import request
 from flask import jsonify
 from thespian.actors import ActorSystem
-from app.enums.actor_name import ActorName
-from app.enums.events_actor_action import EventsActorAction
+from app.enums.events_action import EventsActorAction
 from app.actors.events_actor import EventsActor
 from app.classes.event import Event
 from app.classes.actor_message import ActorMessage
@@ -15,85 +14,103 @@ bp = Blueprint('events', __name__, url_prefix='/api/events')
 def index():
     """Get event list."""
     try:
+        customer_id = request.headers.get('Customer-ID')
         asys = ActorSystem()
-        actor = asys.createActor(EventsActor, None, ActorName.EVENTS_ACTOR)
-        message = ActorMessage(EventsActorAction.EVENTS_LIST, None)
+        actor = asys.createActor(actorClass=EventsActor)
+        message = ActorMessage(action=EventsActorAction.EVENTS_LIST,
+                               customer_id=customer_id)
+        response = asys.ask(actor, message)
+        if response.error:
+            return jsonify({'error': str(response.error.message)}), response.error.http_code
+        events = response.payload.get('events')
         events_dict = []
-        events = asys.ask(actor, message)
         for event in events:
-            events_dict.append(event.__dict__)
-        return jsonify(events)
+            events_dict.append(Event.to_dict(event))
+        return jsonify(events_dict)
     except Exception as ex:
-        return jsonify({'error': str(ex)})
+        return jsonify({'error': str(ex)}), 500
 
 
 @bp.route('/', methods=['POST'])
 def add():
-    """Add new event."""
+    """Add a new event."""
     try:
+        customer_id = request.headers.get('Customer-ID')
+        if customer_id:
+            return jsonify({'error': "You do not have permissions."}), 403
         asys = ActorSystem()
-        actor = asys.createActor(EventsActor, None, ActorName.EVENTS_ACTOR)
+        actor = asys.createActor(actorClass=EventsActor)
         event = Event.from_json(request.get_json())
         payload = {
             'event': event
         }
-        message = ActorMessage(EventsActorAction.EVENTS_ADD, payload)
-        asys.tell(actor, message)
+        message = ActorMessage(action=EventsActorAction.EVENTS_ADD,
+                               payload=payload)
+        response = asys.ask(actor, message)
+        if response.error:
+            return jsonify({'error': str(response.error.message)}), response.error.http_code
         return '', 204
     except Exception as ex:
-        return jsonify({'error': str(ex)})
+        return jsonify({'error': str(ex)}), 500
 
 
 @bp.route('/<event_id>', methods=['GET'])
 def get(event_id):
     """Get event by ID."""
     try:
+        customer_id = request.headers.get('Customer-ID')
         asys = ActorSystem()
-        actor = asys.createActor(EventsActor, None, ActorName.EVENTS_ACTOR)
+        actor = asys.createActor(actorClass=EventsActor)
         payload = {
             'event_id': int(event_id)
         }
-        message = ActorMessage(EventsActorAction.EVENTS_GET, payload)
-        event = asys.ask(actor, message)
-        return jsonify(event.__dict__)
+        message = ActorMessage(action=EventsActorAction.EVENTS_GET,
+                               payload=payload, customer_id=customer_id)
+        response = asys.ask(actor, message)
+        if response.error:
+            return jsonify({'error': str(response.error.message)}), response.error.http_code
+        return jsonify(Event.to_dict(response.payload.get('event')))
     except Exception as ex:
-        return jsonify({'error': str(ex)})
+        return jsonify({'error': str(ex)}), 500
 
 
-@bp.route('/<event_id>/tickets', methods=['GET'])
-def get_tickets(event_id):
-    """Get tickets for event by event ID."""
+@bp.route('/sales', methods=['GET'])
+def get_sales():
+    """Get the number of tickets sold per event."""
     try:
+        customer_id = request.headers.get('Customer-ID')
+        if customer_id:
+            return jsonify({'error': "You do not have permissions."}), 403
         asys = ActorSystem()
-        actor = asys.createActor(EventsActor, None, ActorName.EVENTS_ACTOR)
-        payload = {
-            'event_id': int(event_id),
-            'order_date': request.args.get('order_date', default=None, type=int),
-            'event_date': request.args.get('event_date', default=None, type=int)
-        }
-        message = ActorMessage(EventsActorAction.EVENTS_TICKETS, payload)
-        events = asys.ask(actor, message)
-        events_dict = []
-        events = asys.ask(actor, message)
-        for event in events:
-            events_dict.append(event.__dict__)
-        return jsonify(events)
+        actor = asys.createActor(actorClass=EventsActor)
+        message = ActorMessage(action=EventsActorAction.EVENTS_SALES)
+        response = asys.ask(actor, message)
+        if response.error:
+            return jsonify({'error': str(response.error.message)}), response.error.http_code
+        return jsonify(response.payload.get('sales_dict'))
     except Exception as ex:
-        return jsonify({'error': str(ex)})
+        return jsonify({'error': str(ex)}), 500
 
 
 @bp.route('/<event_id>/purchase', methods=['POST'])
 def purchase(event_id):
-    """Purchase tickets for the event."""
+    """Purchase tickets for a specific event."""
     try:
+        customer_id = request.headers.get('Customer-ID')
+        if not customer_id:
+            return jsonify({'error': "You do not have permissions."}), 403
         asys = ActorSystem()
-        actor = asys.createActor(EventsActor, None, ActorName.EVENTS_ACTOR)
+        actor = asys.createActor(actorClass=EventsActor)
+        customer_id = request.headers.get('Customer-ID')
         payload = {
             'event_id': int(event_id),
-            'quantity': request.get_json().get('quantity')
+            'quantity': int(request.get_json().get('quantity'))
         }
-        message = ActorMessage(EventsActorAction.EVENTS_PURCHASE, payload)
-        asys.tell(actor, message)
+        message = ActorMessage(action=EventsActorAction.EVENTS_PURCHASE,
+                               payload=payload, customer_id=customer_id)
+        response = asys.ask(actor, message)
+        if response.error:
+            return jsonify({'error': str(response.error.message)}), response.error.http_code
         return '', 204
     except Exception as ex:
-        return jsonify({'error': str(ex)})
+        return jsonify({'error': str(ex)}), 500
