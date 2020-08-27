f635045852473a380ac78effd0e38233e3b58011
Robin Genz <mail@robingenz.de>
2020-08-27 15:25:16 +0000

    chore(client): add missing init files

--- src/client/app/utils/date.py
+++ src/client/app/utils/date.py
@@ -1,14 +1,15 @@
 from datetime import datetime
 
-class DateHelper():
-    
+
+class DateHelper:
+
     @staticmethod
     def date_to_timestamp(date):
         input_date = datetime.strptime(date, '%d.%m.%Y')
         timestamp = input_date.timestamp()
         return timestamp
 
-    @staticmethod    
+    @staticmethod
     def timestamp_to_date(timestamp):
         date = datetime.fromtimestamp(timestamp)
         return date.strftime('%d.%m.%Y')
